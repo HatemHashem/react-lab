337910444d42d9483b3f329e20c0b459780c92b7
hosam <houssamali94@gmail.com>
2021-03-20 13:07:13 +0200

    show form results

--- test-react/src/components/EventForm.js
+++ test-react/src/components/EventForm.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 export class EventForm extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   state = {
     events: [],
-    
+
     newEvent: {
       id: "",
       name: "",
@@ -16,66 +16,103 @@ export class EventForm extends Component {
       location: "",
       date: "",
       hotel: "",
-      codeOfConduct: ""
+      codeOfConduct: "",
     },
-   
   };
   updateEvent(key, value) {
     this.setState({ newEvent: { ...this.state.newEvent, [key]: value } });
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
     console.log(this.state.newEvent);
-    this.updateEvent("id",  Date.now());
-    this.setState({events: this.state.events.concat(this.state.newEvent)});
+    this.updateEvent("id", Date.now());
+    this.setState({ events: this.state.events.concat(this.state.newEvent) });
     console.log(this.state.events);
   }
 
-
-
   render() {
     return (
-      <form onSubmit= {this.handleSubmit}>
-        <input
-          className="input"
-          name=""
-          value={this.state.newEvent.description}
-          onChange={(e) => this.updateEvent("description", e.target.value)}
-          type="text"
-        />
-        <input
-          className="input"
-          name=""
-          value={this.state.newEvent.location}
-          onChange={(e) => this.updateEvent("location", e.target.value)}
-          type="text"
-        />
-        <input
-          className="input"
-          name=""
-          value={this.state.newEvent.date}
-          onChange={(e) => this.updateEvent("date", e.target.value)}
-          type="text"
-        />
-        <input
-          className="input"
-          name=""
-          value={this.state.newEvent.hotel}
-          onChange={(e) => this.updateEvent("hotel", e.target.value)}
-          type="text"
-        />
-         <input
-          className="input"
-          name=""
-          value={this.state.newEvent.codeOfConduct}
-          onChange={(e) => this.updateEvent("codeOfConduct", e.target.value)}
-          type="text"
-        />
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">Name:</label>
+          <br></br>
 
-        <button >Add #{this.state.events.length + 1}</button>
+          <input
+            className="input"
+            name=""
+            value={this.state.newEvent.name}
+            onChange={(e) => this.updateEvent("name", e.target.value)}
+            type="text"
+          />
+          <br></br>
+          <label htmlFor="">Description:</label>
+          <br></br>
+          <input
+            className="input"
+            name=""
+            value={this.state.newEvent.description}
+            onChange={(e) => this.updateEvent("description", e.target.value)}
+            type="text"
+          />
+          <br></br>
+          <label htmlFor="">Location:</label>
+          <br></br>
+          <input
+            className="input"
+            name=""
+            value={this.state.newEvent.location}
+            onChange={(e) => this.updateEvent("location", e.target.value)}
+            type="text"
+          />
+          <br></br>
+          <label htmlFor="">Date:</label>
+          <br></br>
+          <input
+            className="input"
+            name=""
+            value={this.state.newEvent.date}
+            onChange={(e) => this.updateEvent("date", e.target.value)}
+            type="text"
+          />
+          <br></br>
 
-      </form>
+          <label htmlFor="">Hotel:</label>
+          <br></br>
+          <input
+            className="input"
+            name=""
+            value={this.state.newEvent.hotel}
+            onChange={(e) => this.updateEvent("hotel", e.target.value)}
+            type="text"
+          />
+          <br></br>
+          <label htmlFor="">code Of Conduct:</label>
+          <br></br>
+          <input
+            className="input"
+            name=""
+            value={this.state.newEvent.codeOfConduct}
+            onChange={(e) => this.updateEvent("codeOfConduct", e.target.value)}
+            type="text"
+          />
+          <br></br>
+
+          <button>Add #{this.state.events.length + 1}</button>
+        </form>
+        <ul>
+          {this.state.events.map((item) => (
+            <li>
+              <span>{item.name} </span>
+              <span>{item.description} </span>
+              <span>{item.location} </span>
+              <span>{item.date} </span>
+              <span>{item.hotel} </span>
+              <span>{item.codeOfConduct} </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
